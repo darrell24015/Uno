47c1a9dcfafe4cd024c8e3fb6e4329a21ef9bdfc
Darrell Little <darrell.little@gmail.com>
2015-04-07 13:56:05 -0400

    First Sketch

---
+++ blink/blink.ino
@@ -0,0 +1,24 @@
+/*
+  blink
+  http://arduino.cc/en/tutorial/blink
+  Turns a LED on and off repeatedly
+  This example code is in the public domain
+*/
+
+// Connect the anode of the LED to pin 13, assign pin 13 to variable led
+// Connect the cathode of the LED to GND
+int led = 13;
+
+// The setup routine runs once when you press the reset button
+void setup() {
+  // initialize the pin as output
+  pinMode(led, OUTPUT);
+}
+
+// The loop will repeat indefinately
+void loop() {
+  digitalWrite(led, HIGH); // Turn the led on, HIGH = 5V 
+  delay(500); // Wait time
+  digitalWrite(led, LOW); // Turn the led off, LOW = 0V
+  delay(500); // Wait time
+}
